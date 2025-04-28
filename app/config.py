87354f9b00310ec10baca3ade3d81ede030ed768
gudokk <masagudkova0@gmail.com>
2025-04-28 11:39:02 +0700

    load

---
+++ app/config.py
@@ -0,0 +1,12 @@
+# app/config.py
+
+SECRET_KEY = "my_super_secret_key"
+ALGORITHM = "HS256"
+
+db_params = {
+    "dbname": "ski_portal",
+    "user": "postgres",
+    "password": "nikita",
+    "host": "localhost",
+    "port": "5432"
+}
